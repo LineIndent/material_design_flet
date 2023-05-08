a09d831f8378d2a16d93c7a8d6c3b4a994ec9d58
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2023-05-08 20:34:32 +0300

    Changes to fm.Checkbox class

--- flet_material/checkbox.py
+++ flet_material/checkbox.py
@@ -4,11 +4,13 @@ import time
 
 
 class CheckBox(ft.Container, Theme):
-    def __init__(self, shape: str, *args, **kwargs):
+    def __init__(self, shape: str, value: bool, disabled: bool, *args, **kwargs):
         self.checkbox: ft.Control = ft.Checkbox(
             fill_color=Theme.primary_theme,
             check_color="white",
             scale=ft.Scale(0.95),
+            value=value,
+            disabled=disabled,
             on_change=lambda e: self.animate_checkbox(e),
         )
 
@@ -17,15 +19,15 @@ class CheckBox(ft.Container, Theme):
         kwargs.setdefault("shape", ft.BoxShape(shape))
         kwargs.setdefault("bgcolor", Theme.primary_theme)
         kwargs.setdefault("content", self.checkbox)
-        kwargs.setdefault("scale", 0.9)
+        kwargs.setdefault("scale", 0.8)
         kwargs.setdefault("animate_scale", ft.Animation(500, "bounceOut"))
         kwargs.setdefault("on_click", lambda e: self.animate_checkbox(e))
 
         super().__init__(*args, **kwargs)
 
     def animate_checkbox(self, e):
-        self.scale = ft.Scale(0.85)
+        self.scale = ft.Scale(0.65)
         self.update()
         time.sleep(0.15)
-        self.scale = ft.Scale(1)
+        self.scale = ft.Scale(0.8)
         self.update()
